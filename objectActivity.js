400d3ab303dfb85c3c1afac15e4f6e62dce8fe52
Shiela N. Llevares <llevaresshiela28@gmail.com>
2025-03-26 17:24:30 +0800

    Add files via upload

---
+++ objectActivity.js
@@ -0,0 +1,23 @@
+//3. Working with objects
+
+// Define a car object
+let car = {
+    type: "Honda",
+    model: "Civic",
+    color: "Silver"
+};
+
+// Check the data type of the car variable
+console.log(typeof car); // Output: "object"
+
+// Change the car type
+car.type = "Toyota";
+// Display the updated car object
+console.log(car); 
+
+// Add a new property 'wheels'
+car.wheels = 4;
+// Display the car object with the added property
+console.log(car);
+
+//https://discuss.codecademy.com/t/javascript-practice-arrays-loops-objects-iterators-groceries-exercise-efficiency/810048
