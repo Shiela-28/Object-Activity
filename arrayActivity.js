566bbedf103083dd8ada7771a698eee7e7feb46b
Shiela N. Llevares <llevaresshiela28@gmail.com>
2025-03-26 21:13:10 +0800

    Add files via upload

---
+++ arrayActivity.js
@@ -0,0 +1,23 @@
+// 1. Working with Arrays
+
+// Creates an array of favorite singers.
+let favSingers = ["Sarah", "Taylor Swift", "Regine"]; 
+
+// Accesses and logs the first element (index 0) of the favSingers array.
+console.log(favSingers[0]); // Logs "Sarah"
+
+
+// Creates an array of favorite numbers.
+let favNumbers = [06, 21, 25, 14];
+
+
+// Creates a mixed array containing different data types, including a nested array.
+let mixedArr = ["string", ["otherarray"], 123, true]; 
+
+// Accesses and logs elements of the mixedArr array.  Note that mixedArr[1] is itself an array.
+console.log(mixedArr[0]); // Logs "string"
+console.log(mixedArr[1]); // Logs ["otherarray"]
+console.log(mixedArr[2]); // Logs 123
+console.log(mixedArr[3]); // Logs true
+
+//https://www.w3schools.com/js/js_arrays.asp
